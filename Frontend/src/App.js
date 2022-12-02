6ddeccb60d63ce9b3e495cb37db06ff99cd95463
subhammahanty235 <subhammahanty235@gmail.com>
2022-12-02 19:51:17 +0530

    sign up page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Login from './pages/authentication/login/Login';
+import Signup from './pages/authentication/signup/Signup';
 
 function App() {
   return (
     <div>
       <Login/>
+      <Signup/>
     </div>
   );
 }
--- Frontend/src/pages/authentication/signup/Signup.jsx
+++ Frontend/src/pages/authentication/signup/Signup.jsx
@@ -1,42 +1,42 @@
 import React from 'react'
-import './login.css'
+import './signup.css'
 import login_image from '../../../images/zorin_login.png'
-function Login() {
+function Signup() {
     return (
-        <div className='md'>
+        <div>
             <div className="main">
 
                 <div className="innermain">
+
                     <div className="image">
                         <img src={login_image} alt="login" />
                         {/* <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" alt="image" /> */}
                     </div>
                     <div className="form">
                         <h3 className="login__header">
-                            <span>W</span>elcome Back!!
+                            <span>Joining</span> A new World
                         </h3>
                         <hr />
                         <div class="form__group field">
                             <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                            <label for="name" class="form__label">Username</label>
+                            <label for="name" class="form__label">Full Name</label>
                         </div>
                         <div class="form__group field">
                             <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                            <label for="name" class="form__label">Password</label>
+                            <label for="name" class="form__label">email id</label>
                         </div>
-                        {/* <div class="form__group field">
+                        <div class="form__group field">
                             <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
-                            <label for="name" class="form__label">Name</label>
-                        </div> */}
-
-                        <button className="btn-login">
-                            Log in
-                        </button>
-                        <button className="btn-signup">
-                            New User? Sign Up
-                        </button>
-
-                        {/* <div className="buttonbox">
+                            <label for="name" class="form__label">username</label>
+                        </div>
+                        <div class="form__group field">
+                            <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
+                            <label for="name" class="form__label">password</label>
+                        </div>
+                        {/* <p className="text-center">
+                            Aleady A user , Click on the Already User Button to login 
+                        </p> */}
+                        <div className="buttonbox">
                             <button className="btn-login1">
                                 Sign Up
                             </button>
@@ -44,13 +44,15 @@ function Login() {
                                Already an User!
                             </button>
 
-                        </div> */}
+                        </div>
                     </div>
+
+                    {/* <button className="btn-su">Login</button> */}
                 </div>
             </div>
         </div>
     )
 }
 
-export default Login
+export default Signup
 
