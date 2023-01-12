5dd4c15cb5f32801edc8512e26d968f9f78a78f7
subhammahanty235 <subhammahanty235@gmail.com>
2023-01-12 18:22:10 +0530

    image reader implemented

--- Frontend/src/pages/mainapp/home/Profiles/UpdateProfile.jsx
+++ Frontend/src/pages/mainapp/home/Profiles/UpdateProfile.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useRef ,useState } from 'react'
 import './profile.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+
 function UpdateProfile() {
+    const fileInput = useRef(null);
+    
+    const [imageSrc, setImageSrc] = useState('https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png');
+
+    // function to call the api
+    const updateprofile = ()=>{
+
+    }
+    const handleClick = () => {
+        fileInput.current.click();
+    };
+
+    const handleChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            setImageSrc(e.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
     return (
         <div>
             <div className="container">
                 <div className="mainscreen">
                     <div className="navbar110">
-                        <button>back</button>
+                        {/* <button>back</button> */}
+                        <FontAwesomeIcon icon={faArrowLeft} />
                         <button className='updatebutton'>Update</button>
                     </div>
                     <div className="profileform">
                         <div className="imagesection">
-                            <img src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png" alt="" className="dp987" />
+                            <img src={imageSrc} onClick={handleClick} alt="" className="dp987" />
+                            <input type="file" ref={fileInput} style={{ display: 'none' }} onChange={handleChange} />
+                            <label>Click on the image to change</label>
 
                         </div>
                         <div className="form2">
@@ -24,7 +51,7 @@ function UpdateProfile() {
                             </div>
                             <div className="groupinp">
 
-                                
+
                                 <div className="innergroupinp">
                                     <label htmlFor="">Username</label>
                                     <div className="usernameinpandbutton">
@@ -40,7 +67,7 @@ function UpdateProfile() {
                                 <div className="innergroupinp">
                                     <label htmlFor="">Bio</label>
                                     {/* <input type="text" name="" id="" /> */}
-                                    <textarea name="" id=""  rows="5"></textarea>
+                                    <textarea name="" id="" rows="5"></textarea>
 
                                 </div>
                             </div>
